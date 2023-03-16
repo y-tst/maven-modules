0f0657dfde12a49cd647780fe0f6a7ddbb95c79e
yurii_yanchuk <yurii_yanchuk@epam.com>
2023-03-17 00:19:50 +0200

    added service using in app

--- application/src/main/java/edu/javamentoring/application/Demo.java
+++ application/src/main/java/edu/javamentoring/application/Demo.java
@@ -1,6 +1,7 @@
 package edu.javamentoring.application;
 
 import edu.javamentoring.cloud.service.impl.CreateUserImpl;
+import edu.javamentoring.cloud.service.impl.ServiceImpl;
 import edu.javamentoring.dto.BankCardType;
 import edu.javamentoring.dto.User;
 import edu.javamentoring.cloud.bank.impl.BankImpl;
@@ -12,10 +13,15 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        CreateUserImpl createUser = new CreateUserImpl();
-        User brandNewUser = createUser.newUser();
 
+        var bankImpl = new BankImpl();
+        var service = new ServiceImpl();
+        var createUser = new CreateUserImpl();
         var scanner = new Scanner(System.in);
+
+        System.out.println("Creating a User:");
+
+        User brandNewUser = createUser.newUser();
 
         System.out.println("Which Card type do you prefer? Credit - input 'c', Debit - input 'd': ");
         var cardType = scanner.nextLine();
@@ -28,11 +34,19 @@ public class Demo {
         } else
             throw new IllegalArgumentException("Incorrect Card type");
 
-        var bankImpl = new BankImpl();
-
         var createdBankCard = bankImpl.createBankCard(brandNewUser, bankCardType);
 
         System.out.println("Brand new card for " + createdBankCard.getUser() + " was issued.");
         System.out.println("The Card number is  " + createdBankCard.getNumber());
+
+        service.subscribe(createdBankCard);
+
+        System.out.println("The subscription was done");
+
+        System.out.println("Performing the check: ");
+
+        service.getAllUsers().forEach(System.out::println);
+        service.getSubscriptionByBankCardNumber(createdBankCard.getNumber())
+                .ifPresentOrElse(System.out::println, () -> System.out.println("User has no subscriptions."));
     }
 }
